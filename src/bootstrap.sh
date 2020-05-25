1d3a6b8a3660d116ff2a368bf302105d213fda9b
James Rocker <james.rocker@simplybusiness.co.uk>
2020-05-25 19:36:26 +0100

    added an auto post request on the flask server to show that data is hosted on it

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,3 +1,12 @@
 export FLASK_APP=./src/main.py
 source venv/bin/activate
 flask run -h 0.0.0.0
+curl -X POST -H 'Content-Type: application/json' -d '{
+  "importance": "Extreme",
+  "description": "An alarm that will wake me up"
+}'
+curl -X POST -H 'Content-Type: application/json' -d '{
+  "importance": "Low",
+  "description": "An alarm to check my emails"
+}'
+ng serve
